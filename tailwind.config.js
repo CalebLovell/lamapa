2842bc4151ce4f3d6cce0dde1903de45a225b7c2
CalebLovell <caleblovell1@gmail.com>
2022-01-13 18:59:09 -0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { fontFamily } = require(`tailwindcss/defaultTheme`);
+const { orange } = require(`tailwindcss/colors`);
+
+module.exports = {
+	mode: `jit`,
+	content: [`./src/pages/**/*.{js,ts,jsx,tsx}`, `./src/components/**/*.{js,ts,jsx,tsx}`],
+	darkMode: `class`,
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: [`Inter`, ...fontFamily.sans],
+			},
+			minHeight: {
+				content: `var(--h-content-area)`,
+			},
+			colors: {
+				orange,
+			},
+		},
+	},
+};
