bedf927d38efb44fb10f90c626991503192b217b
CalebLovell <caleblovell1@gmail.com>
2022-04-28 16:54:46 -0500

    make map work

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,30 +1,41 @@
 import * as d3 from 'd3';
 import * as React from 'react';
+import { worldTopojson } from '../data/worldTopojson.js';
 import * as topojson from 'topojson-client';
 
-export function Map() {
-	const svgRef = React.useRef();
+export default function Map() {
+	const mapRef = React.useRef(null);
 
-	const aspect = 1.1;
-	const width = 1000;
-	const height = width * aspect;
+	const width = 800;
+	const height = 1267.2;
 
 	const projection = d3.geoAzimuthalEqualArea().rotate([80, 10]).fitSize([width, height], laGeojson);
 	const path = d3.geoPath(projection);
 
-	const renderChart = (nyc, path) => {
-		d3.select(svgRef.current);
-
-		svg.append(`path`).datum(d3.geoGraticule()).attr(`d`, path).attr(`fill`, `none`).attr(`stroke`, `#f0f0f0`);
-
-		svg.append(`path`).datum(worldGeojsonOuter).attr(`fill`, `white`).attr(`stroke`, `#aaa`).attr(`d`, path);
-
-		svg.selectAll(`.country`).data(laGeojson.features).enter().append(`path`).attr(`class`, `country`).attr(`fill`, `#ddd`).attr(`d`, path);
-
-		svg.append(`path`).datum(worldGeojsonInner).attr(`fill`, `none`).attr(`stroke`, `#eee`).attr(`d`, path);
-
-		svg.node();
+	const renderMap = () => {
+		const svg = d3.select(mapRef.current);
+		svg.append(`path`).datum(worldGeojsonOuter).attr(`fill`, `white`).attr(`stroke`, `red`).attr(`d`, path);
+		// svg.selectAll(`.country`).data(laGeojson.features).enter().append(`path`).attr(`class`, `country`).attr(`fill`, `#ddd`).attr(`d`, path);
+		svg.append(`path`).datum(worldGeojsonInner).attr(`fill`, `none`).attr(`stroke`, `red`).attr(`d`, path);
 	};
 
-	return <svg id='map' ref={svgRef} width={width} height={height}></svg>;
+	React.useEffect(() => {
+		renderMap();
+	}, []);
+
+	return <svg id='map' ref={mapRef} width={width} height={height}></svg>;
 }
+
+const worldGeojson = topojson.feature(worldTopojson, worldTopojson.objects.ne_110m_admin_0_countries_lakes);
+
+const features = worldGeojson.features.filter(
+	d => d.properties.CONTINENT.includes(`America`) && ![`Greenland`, `United States of America`].includes(d.properties.ADMIN),
+);
+
+const laGeojson = {
+	type: `FeatureCollection`,
+	features,
+};
+
+const worldGeojsonInner = topojson.mesh(worldTopojson, worldTopojson.objects.ne_110m_admin_0_countries_lakes, (a, b) => a !== b);
+const worldGeojsonOuter = topojson.mesh(worldTopojson, worldTopojson.objects.ne_110m_admin_0_countries_lakes, (a, b) => a === b);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,13 @@
-// import { MyResponsiveChoropleth } from '@components/Map2';
 import type { NextPage } from 'next';
 import dynamic from 'next/dynamic';
 
-const DynamicMap = dynamic(() => import(`@components/MyResponsiveChoropleth`), {
+const DynamicMap = dynamic(() => import(`@components/Map`), {
 	ssr: false,
 });
 
 const Home: NextPage = () => {
 	return (
 		<div style={{ width: `100vw`, height: `100vh` }}>
-			{/* <DynamicMap x={500} y={500} /> */}
 			<DynamicMap />
 		</div>
 	);
