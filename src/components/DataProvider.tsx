b333ac40687865639a64c8eb5761db681ec5e6ad
CalebLovell <caleblovell1@gmail.com>
2022-05-19 14:17:31 -0500

    add 2 types of color interpolation

--- src/components/DataProvider.tsx
+++ src/components/DataProvider.tsx
@@ -1,21 +1,25 @@
-import { Year, years } from '@data/data';
+import { initialMapColorType, MapColorType, Year, years } from '@data/data';
 import * as React from 'react';
 
-type Action = { type: `SET_YEAR`; payload: Year };
+type Action = { type: `SET_YEAR`; payload: Year } | { type: `SET_MAP_COLOR_TYPE`; payload: MapColorType };
 type Dispatch = (action: Action) => void;
-type State = { year: Year };
+type State = { year: Year; mapColorType: MapColorType };
 
 const DataStateContext = React.createContext<State | undefined>(undefined);
 const DataDispatchContext = React.createContext<Dispatch | undefined>(undefined);
 
 const initialDataState = {
 	year: years[0],
+	mapColorType: initialMapColorType,
 };
 
 const DataReducer = (state: State, action: Action) => {
 	switch (action.type) {
 		case `SET_YEAR`: {
 			return { ...state, year: action.payload };
+		}
+		case `SET_MAP_COLOR_TYPE`: {
+			return { ...state, mapColorType: action.payload };
 		}
 		default: {
 			throw new Error(`Unhandled action type: ${action}`);
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,30 +1,31 @@
 import * as React from 'react';
 import * as d3 from 'd3';
 import * as topojson from 'topojson-client';
-import { worldTopojson as worldGeojson } from '../data/worldGeojson';
-import { Country } from './Country';
-import { useDataState } from './DataProvider';
+import { worldTopojson as worldGeojson } from '@data/worldGeojson';
+import { Country } from '@components/Country';
+import { useDataState } from '@components/DataProvider';
 
 const laTopoJson = topojson.feature(worldGeojson, worldGeojson.objects.countries);
+const height = 800;
+const width = 700;
+const projection = d3.geoAzimuthalEqualArea().rotate([80, 10]).fitSize([width, height], { type: `FeatureCollection`, features: laTopoJson.features });
+const projectionPath = d3.geoPath().projection(projection);
+d3.select(`body`).append(`div`).attr(`id`, `tooltip`).attr(`style`, `position: absolute; opacity: 0`);
 
 export default function Map() {
 	const mapRef = React.useRef(null);
-	const { year } = useDataState();
+	const { year, mapColorType } = useDataState();
 
-	const width = 700;
-	const height = 800;
-
-	const projection = d3
-		.geoAzimuthalEqualArea()
-		.rotate([80, 10])
-		.fitSize([width, height], { type: `FeatureCollection`, features: laTopoJson.features });
-	const path = d3.geoPath().projection(projection);
-
-	d3.select(`body`).append(`div`).attr(`id`, `tooltip`).attr(`style`, `position: absolute; opacity: 0`);
+	const domain = mapColorType === `global` ? [0, 10] : [10, 0];
+	const interpolateColor = d3.scaleSequential(d3.interpolateRdBu).domain(domain);
 
 	const countries = laTopoJson.features.map(data => {
 		const country = year.find(x => x.name === data.properties.ADMIN);
-		return <Country key={data.properties.ADMIN} path={path(data)} tooltipData={data.properties.ADMIN} color={country?.color} />;
+		const path = projectionPath(data);
+		const color = country ? interpolateColor(country.value) : `black`;
+
+		const tooltipData = data.properties.ADMIN;
+		return <Country key={tooltipData} path={path} tooltipData={tooltipData} color={color} />;
 	});
 
 	return (
