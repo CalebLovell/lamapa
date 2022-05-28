23786b71300a6e286f3db9b45cefb59f6e2ae154
CalebLovell <caleblovell1@gmail.com>
2022-05-28 13:33:52 -0500

    update seeders

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { countries } from '../src/data/seeders';
+import { countries, headsOfState } from '../src/data/seeders';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
@@ -10,6 +10,13 @@ async function main() {
 	});
 	const seededCountries = await Promise.all(createManyCountryPromises);
 	console.log({ seededCountries });
+
+	const createManyHeadsOfStatePromises = headsOfState.map(x => {
+		const { countryId, name, politicalLeaning, startDate, endDate } = x;
+		return prisma.headOfState.create({ data: { countryId, name, politicalLeaning, startDate, endDate } });
+	});
+	const seededHeadsOfState = await Promise.all(createManyHeadsOfStatePromises);
+	console.log({ seededHeadsOfState });
 }
 
 main()
